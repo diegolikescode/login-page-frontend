a4ab043edfec860c650fc5369aed3505bbcbdc39
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-01-25 12:45:53 -0300

    adding things and jpg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { FrontPageComponent } from './front-page/front-page.component';
+import { FrontPageComponent } from './front-page/front-page.component'
 import { FooterComponent } from './shared/footer/footer.component'
+import { ButtonComponent } from './shared/button/button.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
     FooterComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
