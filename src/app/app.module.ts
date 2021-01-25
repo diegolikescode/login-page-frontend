61be72cce7a74c9db4709c325a775330364b15b6
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-01-25 11:39:37 -0300

    feat: new assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { FrontPageComponent } from './front-page/front-page.component'
+import { FrontPageComponent } from './front-page/front-page.component';
+import { FooterComponent } from './shared/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
