8bec8e267b0487443737bb983c5700a50271ffb6
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-02-02 10:49:02 -0300

    feat: starting to use the HttpClientModule to consume the api, need to prepare the api first

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  readonly API_URL = 'http://localhost:3333'
 
-  constructor() { }
+  constructor(private http: HttpClientModule) { }
 
   ngOnInit(): void {
   }
